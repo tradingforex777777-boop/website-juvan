e2e05e56dd1d8d09c18d5185d93b62fbe2380071
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-25 02:37:26 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -164,7 +164,7 @@ const Home = () => {
             <motion.img 
               key={currentHeroImage}
               src={heroImages[currentHeroImage]} 
-              initial={{ opacity: 0, scale: 1.1 }}
+              initial={{ opacity: 1, scale: 1.1 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 1.5, ease: "easeInOut" }}
