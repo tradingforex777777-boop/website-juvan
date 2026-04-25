faae281eac70311923bbe07c33fe5c9b812de9e6
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-26 03:15:39 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -716,12 +716,16 @@ const Contact = () => {
         </div>
         <script>
           function sendwhatsapp(){
+            var phonenumber = "+6282312451989";
+          
             var nama = document.queryselector('.nama').value;
             var nomorhp = document.queryselector('.nomorhp').value;
             var email = document.queryselector('.email').value;
             var subjek = document.queryselector('.subjek').value;
             var pesan = document.queryselector('.pesan').value;
-            var url = "https://wa.me/6282312451989" + nama + "?text="
+          
+            var url = "https://wa.me/" + phonenumber + "?text="
+            +"*nama :* " +nama+"%0a"
             +"*nomorhp :* " +nomorhp+"%0a"
             +"*email :* " +email+"%0a"
             +"*subjek :* " +subjek+"%0a"
