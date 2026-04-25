9d73af2d928a99eaf0344f28836336066347d913
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-26 03:22:35 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,25 @@ function ScrollToTop() {
   }, [pathname]);
   return null;
 }
+        <script>
+          function sendwhatsapp(){
+            var phonenumber = "+6282312451989";
+          
+            var nama = document.queryselector('.nama').value;
+            var nomorhp = document.queryselector('.nomorhp').value;
+            var email = document.queryselector('.email').value;
+            var subjek = document.queryselector('.subjek').value;
+            var pesan = document.queryselector('.pesan').value;
+          
+            var url = "https://wa.me/" + phonenumber + "?text="
+            +"*nama :* " +nama+"%0a"
+            +"*nomorhp :* " +nomorhp+"%0a"
+            +"*email :* " +email+"%0a"
+            +"*subjek :* " +subjek+"%0a"
+            +"*pesan :* " +pesan+"%0a%0a"
+          window.open(url, '_blank').focus();      
+          }
+        </script>
 
 export default function App() {
   return (
