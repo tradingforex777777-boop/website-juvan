d20449338dacf0042be2d965f110b68174dfebbd
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-25 15:41:52 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -221,7 +221,7 @@ const Home = () => {
       {/* History Section */}
       <section className="py-24 px-6 bg-sky-50">
         <div className="max-w-7xl mx-auto mb-16 text-center">
-          <h2 className="text-4xl md:text-5xl font-bold text-slate-900 mb-4 tracking-tight uppercase">Sejarah</h2>
+          <h2 className="text-4xl md:text-5xl font-bold border text-white mb-4 tracking-tight uppercase">Sejarah</h2>
           <div className="w-24 h-1.5 bg-primary-dark mx-auto rounded-full"></div>
         </div>
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-16 items-center">
