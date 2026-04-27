ad0eee21b1d8c577c110e7d2d35687dfdcc35662
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-27 17:21:53 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -334,7 +334,7 @@ const About = () => {
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
-              className="lg:col-span-2 bg-primary-dark p-12 rounded-[2.5rem] text-white shadow-2xl flex flex-col justify-center border border-white/10"
+              className="lg:col-span-3 bg-primary-dark p-12 rounded-[2.5rem] text-white shadow-2xl flex flex-col justify-center border border-white/10"
             >
               <div className="mb-10">
                 <h3 className="text-3xl font-bold text-white uppercase">Misi Perusahaan</h3>
