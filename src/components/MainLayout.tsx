4246e8fb4099883c22a0f67f34e49ab8ae0777e0
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-28 06:30:42 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -588,7 +588,8 @@ const Contact = () => {
               </p>
             </div>
           </div>
-
+          
+            <div className="max-w-7xl mx-auto px-6 mb-24 p-8 md:p-16">
               <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 <div className="col-span-2 md:col-span-4 bg-primary-dark p-6 rounded-2xl shadow-xl border border-white/5 group overflow-hidden relative h-50">
                   <div className="absolute top-0 right-0 w-24 h-24 bg-blue-400/10 rounded-bl-full -mr-6 -mt-6 transition-transform group-hover:scale-150 duration-500"></div>
@@ -662,7 +663,8 @@ const Contact = () => {
                   </div>
                 </a>
               </div>
-            </div>          
+            </div>
+          </div>
       </section>
      
       <section className="px-6">
