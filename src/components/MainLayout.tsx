9385e057c4e24d1b9bcb26341cc1c7e7e3c4dd24
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-28 01:45:50 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -607,10 +607,10 @@ const Contact = () => {
             </div>                     
         </div>
           
-          <div className="max-w-7xl mx-auto p-8 md:p-8">
+          <div className="max-w-7xl mx-auto px-6 mb-24">
               <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 <div className="col-span-2 md:col-span-4 bg-primary-dark p-6 rounded-2xl shadow-xl border border-white/5 group overflow-hidden relative h-50">
-                  <div className="absolute top-0 right-0 w-24 h-36 bg-blue-400/10 rounded-bl-full -mr-6 -mt-6 transition-transform group-hover:scale-150 duration-500"></div>
+                  <div className="absolute top-0 right-0 w-24 h-24 bg-blue-400/10 rounded-bl-full -mr-6 -mt-6 transition-transform group-hover:scale-150 duration-500"></div>
                   <div className="flex gap-5 items-start relative z-10">
                     <div className="w-12 h-12 bg-white/10 text-white rounded-xl flex items-center justify-center shrink-0 group-hover:bg-[#25D366] group-hover:text-white transition-all transform group-hover:rotate-12">
                       <MapPin size={24} />
