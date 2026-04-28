6e109329b23faca5392d1388f962c6c644b580e5
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-28 21:35:16 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -602,7 +602,7 @@ const Contact = () => {
               <div className="absolute inset-0 mix-blend-multiply"></div>
             </motion.div>         
         </div>
-            <div className="max-w-7xl mx-auto px-0 mb-24 p-8 md:p-10">
+            <div className="max-w-7xl mx-auto px-6 mb-24 p-8 md:p-10">
               <div className="grid grid-cols-1 md:grid-cols-5 gap-4">  
                 <div className="bg-primary-dark p-4 rounded-2xl shadow-xl border border-white/5 group flex flex-col items-center justify-center text-center relative overflow-hidden h-50">                  
                   <div className="absolute inset-0 bg-gradient-to-br from-green-500/10 to-transparent opacity-0 group-hover:opacity-100 transition-opacity"></div>
@@ -659,7 +659,7 @@ const Contact = () => {
                   </div>
                 </a>
 
-                <div className="bg-primary-dark p-6 rounded-2xl shadow-xl border border-white/5 group overflow-hidden relative h-auto w-80">
+                <div className="bg-primary-dark p-6 rounded-2xl shadow-xl border border-white/5 group overflow-hidden relative h-auto w-auto">
                   <div className="absolute top-0 right-0 w-24 h-24 bg-blue-400/10 rounded-bl-full -mr-6 -mt-6 transition-transform group-hover:scale-150 duration-500"></div>
                   <div className="flex gap-5 items-start relative z-10">
                     <div className="w-12 h-12 bg-white/10 text-white rounded-xl flex items-center justify-center shrink-0 group-hover:bg-[#25D366] group-hover:text-white transition-all transform group-hover:rotate-12">
