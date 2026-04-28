0cf8db9870006ffbae37748d214d1a8b63818170
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-29 00:17:28 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -606,7 +606,7 @@ const Contact = () => {
           </div>
       
                           
-            <div className="px-6 max-w-7xl mx-auto">
+            <div className="pb-24 px-6 max-w-7xl mx-auto">
               <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 <div className="col-span-2 md:col-span-4 bg-primary-dark p-6 rounded-2xl shadow-xl border border-white/5 group overflow-hidden relative h-50">
                   <div className="absolute px-4 top-0 right-0 w-24 h-24 bg-blue-400/10 rounded-bl-full -mr-6 -mt-6 transition-transform group-hover:scale-150 duration-500"></div>
