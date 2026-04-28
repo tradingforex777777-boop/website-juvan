a7973d8a889ec029bf93d421a2253f01385d0498
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-28 21:44:48 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -602,8 +602,8 @@ const Contact = () => {
               <div className="absolute inset-0 mix-blend-multiply"></div>
             </motion.div>         
         </div>
-            <div className="max-w-7xl mx-auto px-6 mb-15 p-8 md:p-10">
-              <div className="grid grid-cols-1 md:grid-cols-5 gap-4">  
+            <div className="max-w-7xl mx-auto px-6 mb-24 p-8 md:p-16">
+              <div className="grid grid-cols-1 md:grid-cols-5 gap-2">  
                 <div className="bg-primary-dark p-4 rounded-2xl shadow-xl border border-white/5 group flex flex-col items-center justify-center text-center relative overflow-hidden w-50">                  
                   <div className="absolute inset-0 bg-gradient-to-br from-green-500/10 to-transparent opacity-0 group-hover:opacity-100 transition-opacity"></div>
                   <div className="relative z-10 flex flex-col items-center">
