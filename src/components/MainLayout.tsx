8d224e0d654ebc445eefe18e7b21cff9fdf66eb8
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-28 21:14:32 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -658,12 +658,7 @@ const Contact = () => {
                     <p className="bg-white/10 text-white px-3 py-1 rounded-md font-bold text-[11px] uppercase tracking-wider hover:bg-[#25D366] transition-all border border-white/5">Visit</p>
                   </div>
                 </a>
-              </div>
-            </div>
-  
-                
-          
-                            
+
                 <div className="col-span-2 md:col-span-4 bg-primary-dark p-6 rounded-2xl shadow-xl border border-white/5 group overflow-hidden relative h-50">
                   <div className="absolute top-0 right-0 w-24 h-24 bg-blue-400/10 rounded-bl-full -mr-6 -mt-6 transition-transform group-hover:scale-150 duration-500"></div>
                   <div className="flex gap-5 items-start relative z-10">
@@ -678,7 +673,14 @@ const Contact = () => {
                       </a>
                     </div>
                   </div>
-                </div>             
+                </div>
+              </div>
+            </div>
+  
+                
+          
+                            
+                             
               
           
     
